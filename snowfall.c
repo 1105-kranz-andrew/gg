b2303beff2407275017404ed7ab53099f1eb00cb
1105-kranz-andrew <157994922+1105-kranz-andrew@users.noreply.github.com>
2024-02-13 14:50:30 -0800

    Create snowfall.c

---
+++ snowfall.c
@@ -0,0 +1,21 @@
+//name:andrew kranz
+//Project 1
+//02/08/24
+#include <stdio.h>
+int main(){
+  double inches = 2.34, 2.40, 2.70;
+  char day's = M, T, W;
+  int date = 1, 2, 3;
+  printf("Enter the first day's data (inches, day, date): Enter the seconds day's data (inches, day, date): Enter the thirds day's (inches , day, date):";
+  scanf(":%d/ninches :%c/nday's :%.2lf/ndate", &2.34 M 1, &2.40 T 2, &2.70 W 3);
+    ***Snowfall DATA***;
+      
+      M 1 = 2.34 inches;
+      
+      T 2 = 2.40 inches;
+      
+      W 3 = 2.70 inches;
+return 0;
+}
+        
+      
